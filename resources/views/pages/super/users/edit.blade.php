702a2ed7033d3733be8d9e9b048822ce74f44f0e
taufiq <cellijo724@gmail.com>
2026-04-20 12:42:26 +0700

    perubahan title users

--- resources/views/pages/super/users/edit.blade.php
+++ resources/views/pages/super/users/edit.blade.php
@@ -1,4 +1,4 @@
-<x-layouts::app :title="__('Daftar Pengguna')">
+<x-layouts::app :title="__('Update Pengguna')">
     <div class="flex flex-col gap-6 w-full">
         <div class="flex items-center justify-between">
             <div>
